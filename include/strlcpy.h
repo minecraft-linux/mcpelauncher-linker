9ddb23479e336ef17b29ff82575231b40524696c
Christopher Homberger <christopher.homberger@web.de>
2020-03-22 20:38:34 +0100

    Experiments

---
+++ include/strlcpy.h
@@ -0,0 +1,10 @@
+#include <string.h>
+extern
+#ifdef __cplusplus
+"C"
+#endif
+#define DEF_WEAK(a)
+#define __BIONIC_ALIGN(__value, __alignment) (((__value) + (__alignment)-1) & ~((__alignment)-1))
+#define	__predict_true(exp)	__builtin_expect((exp) != 0, 1)
+#define	__predict_false(exp)	__builtin_expect((exp) != 0, 0)
+size_t strlcpy(char *dst, const char *src, size_t dsize);
