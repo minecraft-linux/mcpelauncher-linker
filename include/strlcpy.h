694f9781eba1beb720f2235606daffdb73eb46ee
Christopher Homberger <christopher.homberger@web.de>
2020-04-09 12:20:28 +0200

    Update to link libminecraft Added some broken libhybris stuff

--- include/strlcpy.h
+++ include/strlcpy.h
@@ -7,5 +7,5 @@ extern
 #define __BIONIC_ALIGN(__value, __alignment) (((__value) + (__alignment)-1) & ~((__alignment)-1))
 #define	__predict_true(exp)	__builtin_expect((exp) != 0, 1)
 #define	__predict_false(exp)	__builtin_expect((exp) != 0, 0)
-#define gettid() syscall(SYS_gettid)
+// #define gettid() syscall(SYS_gettid)
 size_t strlcpy(char *dst, const char *src, size_t dsize);
