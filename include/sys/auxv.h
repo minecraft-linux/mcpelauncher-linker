5127987ca49c4aeca6d180f26a9a4ac5aa4501c2
Christopher Homberger <christopher.homberger@web.de>
2020-10-20 11:28:02 +0200

    Stub auxv, for arm flatpak / macOS

--- include/sys/auxv.h
+++ include/sys/auxv.h
@@ -4,3 +4,13 @@
 #ifdef __linux__
 #include_next <sys/auxv.h>
 #endif
+#if (defined(__aarch64__) || defined(__arm__)) && (!defined(AT_HWCAP) || !defined(AT_HWCAP2))
+#ifndef AT_HWCAP
+#define AT_HWCAP 0
+#endif
+#ifndef AT_HWCAP2
+#define AT_HWCAP2 0
+#endif
+#define getauxval(a) 0
+#pragma message "stubbed getauxval"
+#endif
