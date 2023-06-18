e297528007954aea6b62eec77f7c0611f453b029
Christopher Homberger <christopher.homberger@web.de>
2023-06-18 20:15:50 +0200

    win32 basic working

--- src/linker-test.cpp
+++ src/linker-test.cpp
@@ -1,9 +1,19 @@
 typedef long long off64_t;
 
+#include <fcntl.h>
 #include <mcpelauncher/linker.h>
 
+__attribute__((sysv_abi)) static void __abort() {
+    abort();
+}
+
 int main() {
+    _set_fmode(_O_BINARY);
     linker::init();
+    std::unordered_map<std::string, void *> syms;
+    syms["__cxa_finalize"] = reinterpret_cast<void*>(&__abort);
+    syms["__cxa_atexit"] = reinterpret_cast<void*>(&__abort);
+    linker::load_library("libc.so", syms);
     auto handle = linker::dlopen(".\\test.so", 0);
     if(!handle) {
         printf("linker::dlerror %s\n", linker::dlerror());
